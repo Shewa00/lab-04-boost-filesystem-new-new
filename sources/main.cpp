4031e9dc34f911bddbce6693801cb302e002a6cc
ARAOvsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2019-12-12 23:49:47 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,3 +1,4 @@
+//Copyright 2019 ARAOvsepyan
 #include "FTPdriver.h"
 
 using boost::filesystem::all_all;
@@ -6,5 +7,4 @@ int main(int argc, char* argv[]) {
     FTPdriver result(argc == 2 ? boost::filesystem::path(argv[1])
                                : boost::filesystem::current_path());
     std::cout << result.FTPresult() << std::endl;
-
 }
